dc9a6f78ed060d0059df91c3880d8855a4ce4520
zhangjian02 <zhangjian02@finupgroup.com>
2018-08-05 00:52:27 +0800

    first commit

---
+++ src/main/java/com/zjrodger/BootServerApplication.java
@@ -0,0 +1,24 @@
+package com.zjrodger;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ImportResource;
+
+@SpringBootApplication
+@ImportResource("classpath:applicationContext.xml")
+@EnableAutoConfiguration(exclude = {
+        org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration.class,
+        org.activiti.spring.boot.SecurityAutoConfiguration.class,
+})
+@ComponentScan
+public class BootServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootServerApplication.class, args);
+        System.out.println("SpringBoot应用启动成功。");
+    }
+
+}
---
+++ src/main/java/com/zjrodger/modules/system/controller/SystemController.java
@@ -0,0 +1,15 @@
+package com.zjrodger.modules.system.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class SystemController {
+    @RequestMapping("/index")
+    public String index() {
+        return "index";
+    }
+}
+
+
