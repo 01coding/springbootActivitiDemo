d1045cb41f670e553c6523e4fa03ab870d6b3f20
zhangjian02 <zhangjian02@finupgroup.com>
2018-08-05 10:18:46 +0800

    增加测试页面

--- src/main/java/com/zjrodger/modules/system/controller/SystemController.java
+++ src/main/java/com/zjrodger/modules/system/controller/SystemController.java
@@ -6,10 +6,34 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class SystemController {
+
     @RequestMapping("/index")
     public String index() {
         return "index";
     }
+
+    @RequestMapping("/userWindow")
+    public String userWindow() {
+        return "activiti/userWindow";
+    }
+
+
+    @RequestMapping("/busTree")
+    public String busTree() {
+        return "activiti/busTree";
+    }
+
+    @RequestMapping("/doActTask")
+    public String doActTask() {
+        return "activiti/doActTask";
+    }
+
+    @RequestMapping("/extendActModelList")
+    public String extendActModelList() {
+        return "activiti/extendActModelList";
+    }
+
+
 }
 
 
