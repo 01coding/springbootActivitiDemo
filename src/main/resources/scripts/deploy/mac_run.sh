b0c2afaf695918b7d8228b728db33b4025f54957
zhangjian02 <zhangjian02@finupgroup.com>
2018-08-05 12:34:21 +0800

    修改POM使得可以，独立打包部署，并且通过shell脚本启动。

---
+++ src/main/resources/scripts/deploy/mac_run.sh
@@ -0,0 +1,5 @@
+java -server  -Xms1g -Xmx1g -Xmn512M -Xss256k -XX:PermSize=128M -XX:MaxPermSize=128M -XX:SurvivorRatio=8 -XX:MaxTenuringThreshold=5 -XX:GCTimeRatio=19 -Xnoclassgc -XX:+DisableExplicitGC -XX:+UseParNewGC -XX:+UseConcMarkSweepGC -XX:+UseCMSCompactAtFullCollection -XX:CMSFullGCsBeforeCompaction=0 -XX:-CMSParallelRemarkEnabled -XX:CMSInitiatingOccupancyFraction=70 -XX:SoftRefLRUPolicyMSPerMB=0 -XX:+PrintClassHistogram -XX:+PrintGCDetails -XX:+PrintGCDateStamps -XX:+PrintHeapAtGC -Xloggc:/Users/apple/data1/springbootActivitiDemo/applogs/gc.log -jar /Users/apple/data1/springbootActivitiDemo/springboot-acitviti-demo-1.0-SNAPSHOT.jar
+>/dev/null 2>&1
+&
+
+java -server -XX:+PrintClassHistogram -XX:+PrintGCDetails -XX:+PrintGCDateStamps -XX:+PrintHeapAtGC -Xloggc:/data1/springbootActivitiDemo/applogs/gc.log -jar /data1/springbootActivitiDemo/springboot-acitviti-demo-1.0-SNAPSHOT.jar
